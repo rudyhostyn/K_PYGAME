e15a435cd687d52168bb6ecd15b95dee331f05bd
rudyhostyn <68102881+rudyhostyn@users.noreply.github.com>
2021-05-05 22:26:51 +0200

    clase raqueta y clase marcador

--- ball_2.py
+++ ball_2.py
@@ -1,78 +1,89 @@
 import pygame as pg
 import sys
 import random
-
 ANCHO = 800
 ALTO = 600
 FPS = 60
-
-class Marcador():
+class Marcador(pg.sprite.Sprite):
     def __init__(self, x, y, fontsize=25, color=(255,255,255)):
+        super().__init__()
         self.fuente = pg.font.SysFont("Arial", fontsize)
-        self.x = x
-        self.y = y
+        self.text = "0"
         self.color = color
+        self.image = self.fuente.render(str(self.text), True, self.color)
+        self.rect = self.image.get_rect(topleft=(x,y))
+    def update(self):
+        self.image = self.fuente.render(str(self.text), True, self.color)
 
-    def dibuja(self, text, lienzo):
-        image = self.fuente.render(str(text), True, self.color)
-        lienzo.blit(image, (self.x, self.y))
-
+class Raqueta(pg.sprite.Sprite):
+    def __init__ (self, x, y, w=100, h=30):
+        super().__init__()
+        self.image =pg.Surface((w, h), pg.SRCALPHA, 32)
+        pg.draw.rect(self.image, (255, 0, 0), pg.Rect(0, 0, w, h), border_radius=10)
+        self.rect = self.image.get_rect(centerx = x, bottom = y)
+        self.vx = 7
+    def update(self):
+        teclas_pulsadas = pg.key.get_pressed() 
+        if teclas_pulsadas[pg.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.vx
+        if teclas_pulsadas[pg.K_RIGHT] and self.rect.right < ANCHO:
+            self.rect.x += self.vx
 
 class Bola(pg.sprite.Sprite):
     def __init__(self, x, y):
-        # pg.sprite.Sprite.__init__(self) <- o este metodo o el de super
         super().__init__()
         self.image = pg.image.load('./images/ball1.png').convert_alpha()
         self.rect = self.image.get_rect(center=(x,y))
-        self.vx = random.randint(5,10)*random.choice([-1,1])
-        self.vy = random.randint(5,10)*random.choice([-1,1])
-
+        self.vx = random.randint(5, 10) * random.choice([-1, 1])
+        self.vy = random.randint(5, 10) * random.choice([-1, 1])
     def update(self):
         self.rect.x += self.vx
         self.rect.y += self.vy
-
-        if self.rect.left <=0 or self.rect.right >= ANCHO:
-            self.vx *=-1
-        if self.rect.top <=0 or self.rect.bottom >= ALTO:
-            self.vy *=-1
-
+        if self.rect.left <= 0 or self.rect.right >= ANCHO:
+            self.vx *= -1 
+        if self.rect.top <= 0 or self.rect.bottom >= ALTO:
+            self.vy *= -1
 class Game():
     def __init__(self):
         self.pantalla = pg.display.set_mode((ANCHO, ALTO))
         self.botes = 0
-        self.cuentaGolpes = Marcador(10,10)
 
-        self.ballGroup = pg.sprite.Group()
-        for i in range(random.randint(1, 20)):
-            bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
-            self.ballGroup.add(bola)
+        self.todoGrupo = pg.sprite.Group()
 
-        self.bola = Bola(ANCHO//2 , ALTO // 2)
-
+        self.cuentaSegundos = Marcador(10,10)
+        self.todoGrupo.add(self.cuentaSegundos)
+        
+        self.bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
+        self.todoGrupo.add(self.bola)
+        
+        self.raqueta = Raqueta(x = ANCHO//2, y = ALTO - 40)
+        self.todoGrupo.add(self.raqueta)
+    
     def bucle_principal(self):
         game_over = False
         reloj = pg.time.Clock()
-        while not game_over:
-            reloj.tick(FPS)
-
+        contador_milisegundos = 0
+        segundero = 0
+        while not game_over: 
+            dt = reloj.tick(FPS)
+            contador_milisegundos += dt
+            
+            if contador_milisegundos >= 1000:
+                segundero += 1
+                contador_milisegundos = 0
+            
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
                     game_over = True
-            self.ballGroup.update()
-
+            
+            self.cuentaSegundos.text = segundero
+            self.todoGrupo.update()
+            
             self.pantalla.fill((0,0,0))
-            self.cuentagolpes.dibuja('Hola', self.pantalla)
-            self.ballGroup.draw(self.pantalla)
-
+            self.todoGrupo.draw(self.pantalla)
+            
             pg.display.flip()
-
 if __name__ == '__main__':
     pg.init()
     game = Game()
     game.bucle_principal()
-    
-
-
-
-
-
